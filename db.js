0fa012ce045625ddd0686d44d10865219e2fdcf3
Selnet54 <selnet54@gmail.com>
2025-12-30 12:11:33 +0100

    Initialize IndexedDB for ZaliheDB

---
+++ db.js
@@ -0,0 +1,23 @@
+let db;
+
+const request = indexedDB.open("ZaliheDB", 1);
+
+request.onupgradeneeded = event => {
+  db = event.target.result;
+
+  if (!db.objectStoreNames.contains("zalihe")) {
+    db.createObjectStore("zalihe", {
+      keyPath: "id",
+      autoIncrement: true
+    });
+  }
+};
+
+request.onsuccess = event => {
+  db = event.target.result;
+  prikaziZalihe();
+};
+
+request.onerror = () => {
+  alert("Greška pri otvaranju baze");
+};
