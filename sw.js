f68d277e4d7058fb46ba814648a86347c7adc1a1
Selnet54 <selnet54@gmail.com>
2025-12-30 12:42:18 +0100

    Update service worker caching implementation
    Refactor service worker to use CACHE_NAME and urlsToCache for caching strategy.

--- sw.js
+++ sw.js
@@ -1,17 +1,40 @@
-const CACHE = "zalihe-v1";
+const CACHE_NAME = "zalihe-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./app.html",
+  "./style.css",
+  "./app.js",
+  "./db.js",
+  "./manifest.json",
+  "./icons/192.jpg",
+  "./icons/512.jpg"
+];
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache =>
-      cache.addAll([
-        "./",
-        "./index.html",
-        "./app.html",
-        "./style.css",
-        "./app.js",
-        "./db.js",
-        "./manifest.json"
-      ])
+// Instalacija SW i keširanje fajlova
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Aktivacija SW i čišćenje starih keševa
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
     )
+  );
+});
+
+// Fetch handler - vraća iz keša ili sa mreže
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
